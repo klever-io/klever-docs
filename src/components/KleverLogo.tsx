c56be242df3bc04434bef65aae97b042391bb3d0
eminivince <communicatti@gmail.com>
2025-09-02 02:01:52 +0100

    update klever wallet-extension and icons url

---
+++ src/components/KleverLogo.tsx
@@ -0,0 +1,54 @@
+export function KleverLogo(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        d="M23.6552 23.5585H14L23.6552 13.9999V23.5585Z"
+        
+      />
+      <path
+        opacity="0.14"
+        d="M23.6552 23.5585L18.8276 18.7309L19.6966 17.9585L23.6552 23.5585Z"
+
+      />
+      <path
+        d="M23.6552 23.5585H4.44141V4.34473L23.6552 23.5585Z"
+
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_568_2519"
+          x1="18.8276"
+          y1="13.9556"
+          x2="18.8276"
+          y2="23.5777"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="2.71717e-07" stopColor="#FF4681" />
+          <stop offset="1" stopColor="#9B44F6" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_568_2519"
+          x1="21.1564"
+          y1="18.5887"
+          x2="19.802"
+          y2="22.1648"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="9.43019e-07" />
+          <stop offset="1" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_568_2519"
+          x1="-0.135918"
+          y1="9.3743"
+          x2="19.1083"
+          y2="28.6185"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="9.43019e-07" stopColor="#FF4681" />
+          <stop offset="0.7392" stopColor="#9B44F6" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
