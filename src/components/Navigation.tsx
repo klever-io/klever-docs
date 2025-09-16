9fc8138b62a57e86f037ab035e7fbcd3feba5334
Renato Ferraz <renatomeloferraz@gmail.com>
2025-09-16 11:21:33 -0300

    remove unused code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -247,11 +247,6 @@ function NavigationGroup({
                       {sections.map((section) => (
                         <li key={section.id}>
                           <NavLink
-                            onClick={() =>
-                              sendGAEvent('event', 'buttonClicked', {
-                                value: 'xyz',
-                              })
-                            }
                             href={`${link.href}#${section.id}`}
                             tag={section.tag}
                             isAnchorLink
