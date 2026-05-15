5d6406d2e18e451c11d95a135f2e43306eb99970
Samuel Façanha <samuel@klever.io>
2026-05-15 14:57:23 -0300

    chore: update next (#76)

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const config = [
+  {
+    ignores: ['.next/**', 'node_modules/**', 'temp/**'],
+  },
+  ...compat.extends('next/core-web-vitals'),
+]
+
+export default config
